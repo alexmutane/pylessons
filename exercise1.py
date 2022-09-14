a0addcaf4e8efcc336f2efd266844898966ebf3a
alex <alex@equipmoz.org>
2022-09-14 15:05:51 +0200

    when will turn 100 years old

--- exercise1.py
+++ exercise1.py
@@ -1,9 +1,12 @@
 
 # Calc year when one will turn 100 years old
+from datetime import date
 username = input("what's your name:")
 userage = input("Tell me your age:")
 userage = int(userage)
+current_year = date.today().year
+birthyear = current_year - userage
+hundred_in = birthyear + 100
 
-birthyear = 2022 - userage
-hundredin = birthyear + 100
-print(username, ", you'll turn 100 years old in: ", hundredin)
+print(username, ", you was born in", birthyear,
+      "and you'll turn 100 years old in:", hundred_in)
